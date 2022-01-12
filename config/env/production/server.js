69f45d8c90c18201ecf971631ab989fec45b8f91
Dae314 <discord@dae314>
2022-01-11 23:09:58 -1000

    Configure server settings

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,7 @@
 module.exports = ({ env }) => ({
-  host: "0.0.0.0",
-  url: env("RENDER_EXTERNAL_URL"),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  url: env('URL', 'http://localhost:1337'),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
