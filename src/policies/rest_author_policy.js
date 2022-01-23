376c2afbb95fdebc542d8baa96b8dd156b5908f0
Dae314 <discord@dae314>
2022-01-22 14:16:31 -1000

    Update error messages

--- src/policies/rest_author_policy.js
+++ src/policies/rest_author_policy.js
@@ -13,11 +13,11 @@ module.exports = async (ctx, config, { strapi }) => {
 
     if (!comp) {
       const sanitized_ctx = {method: ctx.request.method, url: ctx.request.url, body: ctx.request.body};
-      logger.error(`A forbidden error occurred on REST Comp update: ${JSON.stringify(sanitized_ctx)}`);
+      logger.error(`A forbidden error occurred on REST author policy: ${JSON.stringify(sanitized_ctx)}`);
       return false;
     }
   } catch(err) {
-    logger.error(`An error occured on REST Comp update while finding comp: ${JSON.stringify(err)}`);
+    logger.error(`An error occured on REST author policy while finding comp: ${JSON.stringify(err)}`);
     throw new PolicyError(`An error occured on REST author policy while finding comp.`, {policy: 'author_policy'});
   }
   return true;
