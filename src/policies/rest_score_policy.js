3a26284aa1e42f03ed2b9d20d1bbf8bb17b9f577
Dae314 <discord@dae314>
2022-02-01 22:28:03 -1000

    Rename upvote policy to rest_score_policy

--- src/policies/rest_score_policy.js
+++ src/policies/rest_score_policy.js
@@ -3,7 +3,7 @@ const { PolicyError } = require('@strapi/utils').errors;
 // disallow users from setting score in the API (read only field)
 module.exports = async (ctx, config, { strapi }) => {
   if('score' in ctx.request.body.data) {
-    throw new PolicyError(`score cannot be set by the API`, {policy: 'rest_upvote_policy'});
+    throw new PolicyError(`score cannot be set by the API`, {policy: 'rest_score_policy'});
   }
   return true;
 }
