e4d7cbea990cda5426a20f27a3d26c0ada8aff4c
Dae314 <discord@dae314>
2022-02-01 16:43:39 -1000

    Fix check for total_upvotes in request data

--- src/policies/gql_upvote_policy.js
+++ src/policies/gql_upvote_policy.js
@@ -2,7 +2,7 @@ const { PolicyError } = require('@strapi/utils').errors;
 
 // disallow users from setting total_upvotes in the API (read only field)
 module.exports = async (ctx, config, { strapi }) => {
-  if(ctx.args.data.total_upvotes) {
+  if('total_upvotes' in ctx.args.data) {
     throw new PolicyError(`total_upvotes cannot be set by the API`, {policy: 'gql_upvote_policy'});
   }
   return true;
--- src/policies/rest_upvote_policy.js
+++ src/policies/rest_upvote_policy.js
@@ -2,7 +2,7 @@ const { PolicyError } = require('@strapi/utils').errors;
 
 // disallow users from setting total_upvotes in the API (read only field)
 module.exports = async (ctx, config, { strapi }) => {
-  if(ctx.request.body.data.total_upvotes) {
+  if('total_upvotes' in ctx.request.body.data) {
     throw new PolicyError(`total_upvotes cannot be set by the API`, {policy: 'rest_upvote_policy'});
   }
   return true;
