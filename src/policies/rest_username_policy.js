1166251fe8c3764f095bd7d9045f28910d40f81f
Dae314 <discord@dae314>
2022-02-01 16:40:02 -1000

    Fix typo in username policy on REST

--- src/policies/rest_username_policy.js
+++ src/policies/rest_username_policy.js
@@ -1,12 +1,12 @@
 const { PolicyError } = require('@strapi/utils').errors;
 
 module.exports = async (ctx, config, { strapi }) => {
-  if(ctx.request.body.username) {
+  if(ctx.request.body.data.username) {
     const invalidChars = new RegExp('[^A-Za-z0-9-._~ ]');
-    if(invalidChars.test(ctx.request.body.username)) {
+    if(invalidChars.test(ctx.request.body.data.username)) {
       throw new PolicyError(`Username can only contain letters, numbers, spaces, and - . _ ~ characters`, {policy: 'rest_username_policy'});
     }
-    if(ctx.request.body.username.length > 20) {
+    if(ctx.request.body.data.username.length > 20) {
       throw new PolicyError(`Username can be a max of 20 characters`, {policy: 'rest_username_policy'});
     }
   }
