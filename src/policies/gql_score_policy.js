c5d5ae14ed7c42ac3efcb3bedee3a08d5652646d
Dae314 <discord@dae314>
2022-02-04 16:14:14 -1000

    Add field to track number of saves and restrict updating of the field

--- src/policies/gql_score_policy.js
+++ src/policies/gql_score_policy.js
@@ -5,11 +5,14 @@ module.exports = async (ctx, config, { strapi }) => {
   if('score' in ctx.args.data) {
     throw new PolicyError(`score cannot be set by the API`, {policy: 'gql_score_policy'});
   }
-  if('upvotes' in ctx.args.data) {
+  if('upvotes' in ctx.args.data || 'upvoters' in ctx.args.data) {
     throw new PolicyError(`upvotes cannot be set by the API`, {policy: 'gql_score_policy'});
   }
-  if('downvotes' in ctx.args.data) {
+  if('downvotes' in ctx.args.data || 'downvoters' in ctx.args.data) {
     throw new PolicyError(`downvotes cannot be set by the API`, {policy: 'gql_score_policy'});
+  }
+  if('saves' in ctx.args.data || 'saved_users' in ctx.args.data) {
+    throw new PolicyError(`saves cannot be set by the API`, {policy: 'gql_score_policy'});
   }
   return true;
 }
--- src/policies/rest_score_policy.js
+++ src/policies/rest_score_policy.js
@@ -5,11 +5,14 @@ module.exports = async (ctx, config, { strapi }) => {
   if('score' in ctx.request.body.data) {
     throw new PolicyError(`score cannot be set by the API`, {policy: 'rest_score_policy'});
   }
-  if('upvotes' in ctx.request.body.data) {
+  if('upvotes' in ctx.request.body.data || 'upvoters' in ctx.request.body.data) {
     throw new PolicyError(`upvotes cannot be set by the API`, {policy: 'rest_score_policy'});
   }
-  if('downvotes' in ctx.request.body.data) {
+  if('downvotes' in ctx.request.body.data || 'downvoters' in ctx.request.body.data) {
     throw new PolicyError(`downvotes cannot be set by the API`, {policy: 'rest_score_policy'});
+  }
+  if('saves' in ctx.request.body.data || 'saved_users' in ctx.request.body.data) {
+    throw new PolicyError(`saves cannot be set by the API`, {policy: 'rest_score_policy'});
   }
   return true;
 }
