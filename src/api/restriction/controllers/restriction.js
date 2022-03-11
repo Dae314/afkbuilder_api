de28542e603d8033cfa4f2542cc9d008a44c1abb
Dae314 <discord@dae314>
2022-03-10 16:52:56 -1000

    Add new restriction content type to handle banning users from making changes

---
+++ src/api/restriction/controllers/restriction.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  restriction controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::restriction.restriction');
---
+++ src/api/restriction/services/restriction.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * restriction service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::restriction.restriction');
