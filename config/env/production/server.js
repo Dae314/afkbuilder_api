7cddeb7a7f1ec3449bf74d6a72db556a37537fab
Dae314 <discord@dae314>
2022-03-17 16:27:11 -1000

    Prepare project for deploy on render

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,14 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('URL', 'http://localhost:1337'),
+  url: env("RENDER_EXTERNAL_URL"),
+  app: {
+    keys: env.array('APP_KEYS', ['koa.sess', 'koa.sess2']),
+  },
+  cron: {
+    enabled: env.bool('CRON_ENABLED', false),
+    tasks: cronTasks,
+  },
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
