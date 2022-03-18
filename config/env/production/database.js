98cea4f6687c15476b79d03bd48132f87c6396b1
Dae314 <discord@dae314>
2022-03-17 23:34:11 -1000

    Remove DNS troubleshooting code

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,10 +1,4 @@
 const { parse } = require("pg-connection-string");
-var dns = require('dns');
-
-const dnscheck = dns.lookup('dpg-c8pv9nfh8vl7rvmq45gg', function (err, addresses, family) {
-  console.log(`dns err: ${err}`);
-  console.log(`dns address: ${addresses}`);
-});
 
 module.exports = ({ env }) => {
   let { host, port, database, user, password } = parse(env("DATABASE_URL"));
