d62797c659f191ec5f1eeab32aa682f377cd1ca1
Dae314 <discord@dae314>
2022-03-17 22:55:18 -1000

    Additional Render troubleshooting

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -2,12 +2,15 @@ const { parse } = require("pg-connection-string");
 var dns = require('dns');
 
 const dnscheck = dns.lookup('dpg-c8pv9nfh8vl7rvmq45gg', function (err, addresses, family) {
-  console.log(addresses);
+  console.log(`dns err: ${err}`);
+  console.log(`dns address: ${addresses}`);
 });
 
 module.exports = ({ env }) => {
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
+  if(!port) port = env.int('DATABASE_PORT', 5432);
+  console.log(env("DATABASE_URL"));
   console.log(`host: ${host}`);
   console.log(`port: ${port}`);
   console.log(`database: ${database}`);
