2e80074e8a66c5c77a34c1e300706031a96e8d98
Dae314 <discord@dae314>
2022-04-12 08:50:07 -1000

    Add setting for destroyTimeoutMillis to postgres config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -21,8 +21,9 @@ module.exports = ({ env }) => {
       pool: {
         min: 0,
         max: 10,
-        createTimeoutMillis: 8000,
         acquireTimeoutMillis: 8000,
+        createTimeoutMillis: 8000,
+        destroyTimeoutMillis: 8000,
         idleTimeoutMillis: 8000,
         reapIntervalMillis: 1000,
         createRetryIntervalMillis: 100,
