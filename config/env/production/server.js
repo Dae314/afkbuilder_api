0b8ebaecbb4d56e80ffe405acd43f7884c49dcff
Dae314 <discord@dae314>
2022-04-12 07:40:52 -1000

    Change production default host to 0.0.0.0

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,7 +1,7 @@
 const cronTasks = require("../../cron-tasks");
 
 module.exports = ({ env }) => ({
-  host: env('HOST', 'localhost'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   url: env('STRAPI_URL', 'http://localhost:1337'),
   proxy: true,
