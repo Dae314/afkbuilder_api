eaff65b20002459a70f690b434f381fd52ac7bdc
Dae314 <discord@dae314>
2022-04-12 08:48:04 -1000

    Add connection pool timing settings to resolve connection timeout errors

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -18,6 +18,15 @@ module.exports = ({ env }) => {
           rejectUnauthorized: false, // For self-signed certificates
         },
       },
+      pool: {
+        min: 0,
+        max: 10,
+        createTimeoutMillis: 8000,
+        acquireTimeoutMillis: 8000,
+        idleTimeoutMillis: 8000,
+        reapIntervalMillis: 1000,
+        createRetryIntervalMillis: 100,
+      },
       debug: false,
     },
   };
